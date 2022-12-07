ad763301b455ec2b960885c1b57b1f89badc7d35
Alex Dow <alexander.dow@armyfc.onmicrosoft.com>
2022-12-06 19:43:53 -0600

    Finish task one, day two

--- adventofcode2022/solutions/day02/scoreRPS.py
+++ adventofcode2022/solutions/day02/scoreRPS.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+import sys
 
-def scoreRPS(rpsList):
+
+def scoreRPS(opp, you):
     ROCK = 1
     PAPER = 2
     SCISSORS = 3
@@ -9,12 +11,40 @@ def scoreRPS(rpsList):
     DRAW = 3
     WIN = 6
 
-    rps = {"X": "A", "Y": "B", "Z": "C"}
+    score = 0
+    rps = {
+        "A": ROCK, "B": PAPER, "C": SCISSORS,
+        "X": ROCK, "Y": PAPER, "Z": SCISSORS
+    }
+
+    # Check for a draw
+    if rps[opp] == rps[you]:
+        score += DRAW + rps[you]
+    elif (rps[opp] % 3) + 1 == rps[you]:
+        # We want to catch the roll over from SCISSORS losing to ROCK
+        # Modulus 3 means that the opponent picking SCISSORS rolls over to
+        # 0. Adding 1 to account for this being a 1-indexed score list.
+        # If you picked ROCK, those numbers match and you win!
+        score += WIN + rps[you]
+    else:
+        score += LOSE + rps[you]
+
+    if score == 0:
+        # ABC
+        print("Some logic error happened when we got {} {}".format(opp, you))
+        sys.exit(1)
+
+    return score
+
+
+def playRPS(rpsList):
     totalScore = 0
 
     for line in rpsList:
         opp, you = line.split()
-        # TODO: the rest
+        totalScore += scoreRPS(opp, you)
+    
+    return totalScore
 
 
 
